197aac4eb18660cccb662ad128990b4ea5a22830
jlgamez <jlgamezhernandez@gmail.com>
2021-02-15 13:22:59 +0100

    finished project

--- cypress/integration/common/base.page.js
+++ cypress/integration/common/base.page.js
@@ -1,20 +1,50 @@
 export const basePage = {
-    goToLoginPage(){cy.visit('https://www.netflix.com/es-en/login')},
-    goToHomePage(){cy.visit('https://www.netflix.com/browse/')},
-    shouldHaveInUrl(str){ cy.url().should('contain', str)},
-    shouldNotHaveInUrl(str){cy.url().should('not.contain', str)},
-    pageTitleMAtch(title){cy.title().should('eq', title)},
-    refresh(){cy.reload()},
-    killCookies(){cy.clearCookies()},
+    shouldHaveInUrl(str){ cy.url().should('contain', str);},
+    shouldNotHaveInUrl(str){cy.url().should('not.contain', str);},
+    pageTitleMAtch(title){cy.title().should('eq', title);},
+    refresh(){cy.reload();},
+    killCookies(){cy.clearCookies();},
+    keepCookies(){Cypress.Cookies.preserveOnce('SecureNetflixId', 'hasSeenCookieDisclosure', 'lhpuuidh-browse-VVCRB7NARJDMVCTBZ74P3GGWMM', 'profilesNewSession');},
+    checkCurrentPage(str){
+        let currentUrl;
+        cy.url().then((url)=>{
+           currentUrl = url;
+        });
+        if (currentUrl.includes(str)) { 
+            return true;
+        } else { 
+            return false;
+        }
+    },
+    checkInIfProfilePage(){
+        let currentPageisProfile = document.getElementsByClassName('profile-gate-label');
+        let ptofileIsvisible = document.getElementsByClassName('profile-gate-label').offsetParent;
+        cy.wait(1500);
+        if(currentPageisProfile){
+            cy.get('div[data-profile-guid="VVCRB7NARJDMVCTBZ74P3GGWMM"]').click();
+        }
+    },
 };
+
+export const go = {
+    toHomePage () {
+        cy.visit('https://www.netflix.com/browse/');
+    },
+
+    toLoginPage () {cy.visit('https://www.netflix.com/login');}
+}
 
 export const perform = {
     login(user, password){
-        basePage.goToLoginPage();
+        go.toLoginPage();
         cy.findByRole('textbox', { name: /email or phone number/i }).type(user, {force:true});
         cy.findByLabelText(/password/i).type(password, {force:true});
         cy.findByRole('button', { name: /sign in/i }).click();
-        cy.get('div[data-profile-guid="VVCRB7NARJDMVCTBZ74P3GGWMM"]').click();
-    }
+        basePage.checkInIfProfilePage();
+    },
+
+    getElementByXpath(path) {
+        return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      }
 }
 
